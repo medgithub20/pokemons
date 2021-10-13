50a53c1a4eb3a7d0216143050bbfc3d518788dfd
Mohamed Ragued <mohamed.ragued@usherbrooke.ca>
2021-10-12 21:34:38 -0400

    cleaned imports

--- src/main/java/mypokemons/PokemonControllerCrud.java
+++ src/main/java/mypokemons/PokemonControllerCrud.java
@@ -1,10 +1,6 @@
 package mypokemons;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-import org.fluttercode.datafactory.impl.DataFactory;
 
 @RestController
 class PokemonControllerCrud {
--- src/main/java/mypokemons/PokemonControllerList.java
+++ src/main/java/mypokemons/PokemonControllerList.java
@@ -1,15 +1,11 @@
 package mypokemons;
 
-import org.fluttercode.datafactory.impl.DataFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Controller
 public class PokemonControllerList {
